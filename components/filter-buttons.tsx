b826539d967868feddff3d6b9b908491e57caf5f
Emil Bagirov <emil5394@User.local>
2024-11-05 14:49:43 +0400

    fix: filter bug

--- components/filter-buttons.tsx
+++ components/filter-buttons.tsx
@@ -18,8 +18,12 @@ type FilterButtonsProps = {
 export const FilterButtons = ({ onClose, defaultRating, defaultReview }: FilterButtonsProps) => {
   const [ratingOpen, setRatingOpen] = React.useState(false);
   const [reviewOpen, setReviewOpen] = React.useState(false);
-  const [ratingValue, setRatingValue] = React.useState([defaultRating ? parseFloat(defaultRating) :  0]);
-  const [reviewValue, setReyValue] = React.useState([defaultReview ? parseFloat(defaultReview) :  0]);
+  const [ratingValue, setRatingValue] = React.useState([
+    defaultRating ? parseFloat(defaultRating) : 0,
+  ]);
+  const [reviewValue, setReyValue] = React.useState([
+    defaultReview ? parseFloat(defaultReview) : 0,
+  ]);
 
   const ratingRef = useRef<HTMLDivElement>(null);
   const reviewRef = useRef<HTMLDivElement>(null);
@@ -58,7 +62,7 @@ export const FilterButtons = ({ onClose, defaultRating, defaultReview }: FilterB
           className="bg-[#FF9102] hover:bg-[#FF9102]/90 text-white rounded-full px-6"
           onClick={() => setRatingOpen(!ratingOpen)}
         >
-          Reyting{' '}
+          Reyting {(ratingValue[0] ?? 0 > 0) && !ratingOpen ? `(${ratingValue[0]})` : ''}
           {ratingOpen ? (
             <ChevronUp className="ml-2 h-4 w-4" />
           ) : (
@@ -101,7 +105,7 @@ export const FilterButtons = ({ onClose, defaultRating, defaultReview }: FilterB
           className="bg-[#FF9102] hover:bg-[#FF9102]/90 text-white rounded-full px-6"
           onClick={() => setReviewOpen(!reviewOpen)}
         >
-          Rey{' '}
+          Rəy {(reviewValue[0] ?? 0 > 0) && !reviewOpen ? `(${reviewValue[0]})` : ''}
           {reviewOpen ? (
             <ChevronUp className="ml-2 h-4 w-4" />
           ) : (
