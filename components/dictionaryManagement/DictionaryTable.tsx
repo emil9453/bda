4232eec9d3337284c01500b92e3acad7a43aa98f
Djincode <yaqubprviz@gmail.com>
2024-11-19 19:56:49 +0400

    local error fixed

--- components/dictionaryManagement/DictionaryTable.tsx
+++ components/dictionaryManagement/DictionaryTable.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+// import { useState } from 'react'
 import Switch from '../admincomponents/togglebutton'
 import { Pencil, Trash2 } from 'lucide-react'
 import { Toaster } from 'react-hot-toast';
@@ -10,30 +10,30 @@ import { SERVER_URL } from '../constants';
 const response = await axios.get(`${SERVER_URL}/doctor/specialties`);
 const specialities = response.data;
 
-interface DictionaryItem {
-  id: number
-  name: string
-  isActive: boolean
-}
+// interface DictionaryItem {
+//   id: number
+//   name: string
+//   isActive: boolean
+// }
 
 export default function DictionaryTable() {
-  const [items, setItems] = useState<DictionaryItem[]>([
-    { id: 1, name: 'Item 1', isActive: true },
-    { id: 2, name: 'Item 2', isActive: false },
-    { id: 3, name: 'Item 3', isActive: true },
-  ])
+  // const [items, setItems] = useState<DictionaryItem[]>([
+  //   { id: 1, name: 'Item 1', isActive: true },
+  //   { id: 2, name: 'Item 2', isActive: false },
+  //   { id: 3, name: 'Item 3', isActive: true },
+  // ])
 
-  const handleToggle = (id: number) => {
-    setItems(prevItems =>
-      prevItems.map(item =>
-        item.id === id ? { ...item, isActive: !item.isActive } : item
-      )
-    )
-  }
+  // const handleToggle = (id: number) => {
+  //   setItems(prevItems =>
+  //     prevItems.map(item =>
+  //       item.id === id ? { ...item, isActive: !item.isActive } : item
+  //     )
+  //   )
+  // }
 
-  const handleDelete = (id: number) => {
-    setItems(prevItems => prevItems.filter(item => item.id !== id))
-  }
+  // const handleDelete = (id: number) => {
+  //   setItems(prevItems => prevItems.filter(item => item.id !== id))
+  // }
 
   return (
     <section className="flex flex-col px-20 mb-4 mt-24 w-full max-md:px-5 max-md:mt-2 max-md:max-w-full">
@@ -56,13 +56,14 @@ export default function DictionaryTable() {
                     </button>
                     <button 
                       className="text-gray-600 hover:text-red-600"
-                      onClick={() => handleDelete(item.id)}
+                      // onClick={() => handleDelete(item.id)}
                     >
                       <Trash2 size={18} />
                     </button>
-                    <Switch 
-                      onChange={() => handleToggle(item.id)} 
-                      isChecked={item.isActive} 
+                    <Switch isChecked={false} onChange={function (): void {
+                      throw new Error('Function not implemented.');
+                    } }                      // onChange={() => handleToggle(item.id)} 
+                      // isChecked={item.isActive} 
                     />
                   </div>
                 </td>
